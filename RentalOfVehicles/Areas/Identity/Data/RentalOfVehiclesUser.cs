82dfa27f1b0a78e4d2cdf92570dc575e228d5bc7
Felipe Vaz <felipevaz@live.com>
2020-11-26 12:43:49 -0300

    project:

---
+++ RentalOfVehicles/Areas/Identity/Data/RentalOfVehiclesUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace RentalOfVehicles.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the RentalOfVehiclesUser class
+    public class RentalOfVehiclesUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+    }
+}
---
+++ RentalOfVehicles/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RentalOfVehicles.Areas.Identity.Data;
+using RentalOfVehicles.Data;
+
+[assembly: HostingStartup(typeof(RentalOfVehicles.Areas.Identity.IdentityHostingStartup))]
+namespace RentalOfVehicles.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<RentalOfVehiclesContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("RentalOfVehiclesContextConnection")));
+
+                services.AddDefaultIdentity<RentalOfVehiclesUser>(options => 
+                {
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.SignIn.RequireConfirmedAccount = false;
+                }).AddEntityFrameworkStores<RentalOfVehiclesContext>();
+
+            });
+        }
+    }
+}
